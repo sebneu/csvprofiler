8c3af883c742769dc4db6369d56cde74e2b114dc
Sebastian Neumaier <seb.neumaier@gmail.com>
2015-05-11 17:50:31 +0200

    update

--- util/column_classifier.py
+++ util/column_classifier.py
@@ -1,3 +1,5 @@
+import gzip
+import traceback
 import numpy
 from pandas import DataFrame
 from sklearn.feature_extraction.text import CountVectorizer
@@ -15,17 +17,20 @@ HAM = 'ham'
 SPAM = 'spam'
 
  
-def build_data_frame(train_sets, key):
+def build_data_frame(train_sets):
     rows = []
     index = []
+    i = 0
     for classification in train_sets:
         for column in train_sets[classification]:
-            col_string = ' '.join([str(c.value) for c in column['values'] if c.value])
-            if classification == key:
-                rows.append({'text': col_string, 'class': HAM})
-            else:
-                rows.append({'text': col_string, 'class': SPAM})
-            index.append(column['name'])
+            try:
+                col_string = ' '.join([str(c.value) for c in column['values'] if c.value])
+                rows.append({'text': col_string, 'class': classification})
+                index.append(str(i))
+                i += 1
+            except Exception as e:
+                print(column['name'] + ' - ' + str(e))
+                print(traceback.format_exc())
     data_frame = DataFrame(rows, index=index)
     return data_frame
 
@@ -39,7 +44,7 @@ if __name__ == '__main__':
         with open(join(path, h + '.pkl'), 'rb') as handle:
             train_sets[h] = pickle.load(handle)
 
-    data = build_data_frame(train_sets, headers[0])
+    data = build_data_frame(train_sets)
     data = data.reindex(numpy.random.permutation(data.index))
     pipeline = Pipeline([
         ('count_vectorizer',   CountVectorizer(ngram_range=(1, 2))),
@@ -51,12 +56,12 @@ if __name__ == '__main__':
     # build example data
     # country
     ex = 'https%3A%2F%2Fcommondatastorage.googleapis.com%2Fckannet-storage%2F2012-04-17T212635%2Fcommitments-export-final.csv.gz'
-
-    dts = tablemagician.from_path(join('/home/neumaier/csv_stats/groups/country', ex))
-    for dt in dts:
-        at = dt.process(max_lines=100)
-        col_str = ' '.join([str(c.value) for c in at.columns[at.headers.index('country')] if c.value])
-        print col_str
+    with gzip.open(join('/home/sebastian/csv_stats/bandersnatch', ex)) as handle:
+        dts = tablemagician.from_file_object(handle)
+        for dt in dts:
+            at = dt.process(max_lines=100)
+            col_str = ' '.join([str(c.value) for c in at.columns[at.headers.index('Country')] if c.value])
+            print col_str
 
     predictions = pipeline.predict([col_str])
     print 'prediction'
