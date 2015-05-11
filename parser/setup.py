0e428b7d385885127c33a1af5efb2b52bca0ab4d
Sebastian Neumaier <seb.neumaier@gmail.com>
2015-05-11 11:27:50 +0200

    setup.py installer

---
+++ parser/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tablemagician',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='sebastian',
+    author_email='',
+    description='A simple CSV parser based on messytables'
+)
